3a5b4f72663934562deec763405b58492beaa6d4
spacegoing <spacegoing@gmail.com>
2016-02-05 12:24:22 +1100

    Refract Alarm.py: Add Alarm and Behavior Interface

--- Alarm/Alarm.py
+++ Alarm/Alarm.py
@@ -1,21 +1,57 @@
+from datetime import datetime
 import time
-from dateutil.parser import parse as timeParser
 
-def show_Remaining_Time(curr,end):
-    """
 
-    :param curr: datetime.datetime object
-    :param end: datetime.datetime object
-    :return:
-    """
-    time_Remaining = timeParser(str(end)) - timeParser(str(curr))
-    print('\rTime Remaining: %s' % str(time_Remaining).split('.')[0], end ='', flush=True)
+class Alarm:
+    def __init__(self, Behavior):
+        '''
+
+        :param Behavior: instance of implementation of class Behavior
+        :return:
+        '''
+        self.Behavior = Behavior
+
+    def timer(self, seconds=0, end_datetime=0):
+        '''
+        Only accept 1 argument
+        Either sleep seconds Or sleep until end_datetime.
+        :param seconds: int
+        :param end_datetime: datetime.time
+        :return:
+        '''
+        self.Behavior.enter_behavior()
+
+        if seconds != 0:
+            for i in range(seconds):
+                time.sleep(1)
+                self.Behavior.in_timer_behavior()
+        else:
+            while datetime.now().time() < end_datetime:
+                time.sleep(1)
+                self.Behavior.in_timer_behavior()
+
+        self.Behavior.exit_behavior()
+
+class Behavior(object):
+    '''
+    Interface.
+
+    All following methods should be implemented.
+    '''
+
+    def __init__(self):
+        pass
+
+    def enter_behavior(self):
+        pass
+
+    def in_timer_behavior(self):
+        pass
+
+    def exit_behavior(self):
+        pass
+
+
 
 if __name__ == '__main__':
-    import datetime
-    end = '14:59:59'
-    while True:
-        curr = datetime.datetime.now().time()
-        show_Remaining_Time(curr,end)
-        time.sleep(1)
-
+    pass
