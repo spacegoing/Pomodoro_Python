497b61f4433efab2e59a0f7159d3f131f9be5480
spacegoing <spacegoing@gmail.com>
2016-11-26 17:18:08 +1100

    Timetable for DNTG

--- main.py
+++ main.py
@@ -11,20 +11,20 @@ work = "Work"
 relax = "Relax"
 sleep = "Sleep"
 isContinu = True
-rawSchema = [["9:00", work],
+rawSchema = [["8:10", work],
              ["12:00", relax],
-             ["14:00", work],
-             ["18:30", relax],
-             ["19:30", work],
+             ["14:30", work],
+             ["19:00", relax],
+             ["20:30", work],
              ["22:30", sleep]]
 
 Scheme_Type = "Pomodoro"
 pomoParams = dict()
-pomoParams['pomo_Work_Time'] = 30
-pomoParams['pomo_Short_Break'] = 5
+pomoParams['pomo_Work_Time'] = 40
+pomoParams['pomo_Short_Break'] = 8
 # Every pomoLongRelaxPeriod have a long break
-pomoParams['pomo_Long_Break_Period'] = 4
-pomoParams['pomo_Long_Break'] = 10
+pomoParams['pomo_Long_Break_Period'] = 3
+pomoParams['pomo_Long_Break'] = 15
 Scheme_Settings = {"Scheme_Type": Scheme_Type,  # Must Have
                    "Pomo_Params": pomoParams  # Only for Pomodoro
                    }
